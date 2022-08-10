a5732b0af50bebd32a03ed83fdb45c722d2c3e1f
Steve Crass <stevecrass@Saras-MBP.hsd1.ut.comcast.net>
2022-08-10 15:01:57 -0600

    added bmi calculator

--- calculator.js
+++ calculator.js
@@ -18,6 +18,20 @@ app.post("/", function(req, res) {
   res.send("The result is: " + result)
 })
 
+app.get("/bmicalculator", function(req, res) {
+  res.sendFile(__dirname + "/bmiCalculator.html")
+})
+
+app.post("/bmicalculator", function(req, res) {
+
+  var height = parseFloat(req.body.height)
+  var weight = parseFloat(req.body.weight)
+
+  var BMI = (weight * 703) / (height * height) 
+
+  res.send("Your BMI is " + BMI)
+})
+
 app.listen(3000, function(){
   console.log('Server started on port 3000')
 })
